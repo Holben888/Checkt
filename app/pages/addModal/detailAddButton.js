eea1f318d42cf4564bf8dd4d187441ee6d449517
Holben888 <holben888@gmail.com>
2018-06-28 22:21:14 -0400

    Disabled / enabled states added w/ animation

--- app/pages/addModal/detailAddButton.js
+++ app/pages/addModal/detailAddButton.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
 import { colors, grayscaleColors } from '../sharedStyles';
 import Icon from 'react-native-vector-icons/Feather';
 
 export default class DetailAddButton extends Component {
     render() {
+        const { disabled } = this.props;
         return (
-            <View style={styles.container}>
-                <Icon name="arrow-right" size={19} color={colors.white} />
-            </View>
+            <TouchableWithoutFeedback disabled={disabled}>
+                <View style={[styles.container, styles.disabled]}>
+                    <Icon name="arrow-right" size={19} color={grayscaleColors.accent} />
+                    {!disabled && <EnabledButton />}
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 };
 
 const circleRadius = 23;
+class EnabledButton extends Component {
+    constructor() {
+        super();
+        this.state = {
+            sizeAnim: new Animated.Value(0),
+        }
+    }
+    componentDidMount() {
+        Animated.spring(this.state.sizeAnim, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    }
+    async componentWillUnmount() {
+        await Animated.timing(this.state.sizeAnim, {
+            toValue: 0,
+            duration: 200,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    render() {
+        const { sizeAnim } = this.state;
+        const animatedStyles = {
+            transform: [
+                { scaleX: sizeAnim },
+                { scaleY: sizeAnim },
+            ],
+        }
+        return (
+            <Animated.View style={[styles.container, styles.enabled, animatedStyles]}>
+                <Icon name="arrow-right" size={19} color={colors.white} />
+            </Animated.View>
+        )
+    }
+}
+
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.primary,
@@ -22,5 +63,11 @@ const styles = StyleSheet.create({
         borderRadius: circleRadius,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    enabled: {
+        position: 'absolute',
+    },
+    disabled: {
+        backgroundColor: grayscaleColors.background,
     },
 })
--- app/pages/addModal/index.js
+++ app/pages/addModal/index.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Dimensions, TextInput, KeyboardAvoidingView } f
 import Backdrop from './backdrop';
 import { colors, grayscaleColors } from '../sharedStyles';
 import DetailAddButton from './detailAddButton';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -27,11 +28,11 @@ export default class AddModal extends Component {
     onTextInputHeightChange(event) {
         if (event && event.nativeEvent && event.nativeEvent.contentSize) {
             const height = event.nativeEvent.contentSize.height;
-            if (height !== this.state.textInputHeight) {
-                this.setState({
-                    textInputHeight: height,
-                })
-            }
+            const { textInputHeight, textInputHeightAnim } = this.state;
+
+            this.setState({
+                textInputHeight: height,
+            });
         }
     }
     checkForDetailPhrases(input) {
@@ -57,12 +58,14 @@ export default class AddModal extends Component {
     }
     render() {
         const { onCollapse } = this.props;
-        const { textInputHeight, input, detailPhrase } = this.state;
+        const { textInputHeight, input, detailPhrase, textInputHeightAnim } = this.state;
         return (
             <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
                 <Backdrop onCollapse={onCollapse} />
-                <View style={[styles.inputContainer, { height: textInputHeight + 55 }]}>
-                    <TextInput keyboardType="twitter" autoFocus style={styles.textInput}
+                <View style={[styles.inputContainer, { height: textInputHeight + 70 }]}>
+                    <TextInput autoFocus style={styles.textInput}
+                        keyboardType="twitter"
+                        spellCheck={false}
                         multiline={true}
                         onContentSizeChange={(event) => this.onTextInputHeightChange(event)}
                         onChangeText={(input) => this.checkForDetailPhrases(input)}>
@@ -75,7 +78,8 @@ export default class AddModal extends Component {
                         </Text>
                     </TextInput>
                     <View style={styles.buttonContainer}>
-                        <DetailAddButton />
+                        <Icon name="md-more" size={30} style={{ height: 30, paddingRight: 15 }} color={grayscaleColors.tertiary} />
+                        <DetailAddButton disabled={!detailPhrase} />
                     </View>
                 </View>
             </KeyboardAvoidingView>
@@ -93,19 +97,22 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         backgroundColor: colors.white,
-        minHeight: 75,
+        minHeight: 90,
         paddingHorizontal: 15,
-        paddingTop: 5,
+        paddingTop: 10,
         borderTopLeftRadius: 5,
         borderTopRightRadius: 5,
         marginTop: -5,
     },
     textInput: {
         fontFamily: 'OpenSans-Regular',
+        minHeight: 20,
         fontSize: 15,
         color: grayscaleColors.primary,
     },
     buttonContainer: {
-        marginTop: 10,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
     }
 })
